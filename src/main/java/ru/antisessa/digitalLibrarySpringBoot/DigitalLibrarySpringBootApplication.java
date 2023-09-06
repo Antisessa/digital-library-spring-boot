19838e17bcabef2c29305f6cdfa7054efc74c358
Antisessa <118179929+Antisessa@users.noreply.github.com>
2023-09-06 09:48:55 +0300

    rebase old project to spring boot

---
+++ src/main/java/ru/antisessa/digitalLibrarySpringBoot/DigitalLibrarySpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.antisessa.digitalLibrarySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalLibrarySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalLibrarySpringBootApplication.class, args);
+	}
+
+}
